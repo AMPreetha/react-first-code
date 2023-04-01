ab40c88165a3050a5d670533cca6ec8100f11c6f
AMPreetha <127812727+AMPreetha@users.noreply.github.com>
2023-04-01 07:20:34 +0530

    Add files via upload

---
+++ expenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem(){
+    return (
+      <div>
+        <h1>Expense Item</h1>
+        <div>
+          <p>Food Rs 10</p>
+        </div>
+        <div>
+          <p>Petrol Rs 100</p>
+        </div>
+        <div>
+          <p>Movies Rs 200</p>
+        </div>
+      </div>
+    );
+}
+export default ExpenseItem;
